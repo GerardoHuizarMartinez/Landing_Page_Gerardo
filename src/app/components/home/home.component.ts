ce61638e08f3706ea834515ced631e1e3e92f14a
Gerardo Huizar Martinez <lugehuizarma@ittepic.edu.mx>
2024-04-29 04:52:26 -0700

    acomodo de estructuras en landing page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,6 @@ import {MatIcon} from "@angular/material/icon"
 })
 export class HomeComponent {
   Mierda19= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUtlt4GM53xlD5M2XeB7sCr8iGy8SWLCkTkJ-0QSQKg&s";
-  ISSSTE= "https://mir-s3-cdn-cf.behance.net/projects/404/c1c73d166076329.Y3JvcCw5MjEsNzIxLDE4Miww.jpg"
   Anime = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-HGmuh88HFS0hPB8FLTQevcrbMNVBvahlY_AiT_2UA&s";
+  CtrlGastos = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO98HsRIiNS0eoY07ExplTfPj8yntHvx_qFjn2bmTp_g&s"
 }
